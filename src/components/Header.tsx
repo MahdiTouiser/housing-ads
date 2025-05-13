e4ec233e05b51fd75c3ac9c9f4ead03d4ca38e2f
Mahdi Touiserkani <mahditouserkani@yahoo.com>
2025-05-13 23:09:30 +0330

    feat: add Header component with navigation links

---
+++ src/components/Header.tsx
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <header className="bg-gray-800 text-white p-4">
+            <div className="container mx-auto flex justify-between items-center">
+                <h1 className="text-2xl font-bold">Housing Ads</h1>
+                <nav>
+                    <ul className="flex space-x-6">
+                        <li><Link to="/" className="hover:text-gray-400">Home</Link></li>
+                        <li><Link to="/login" className="hover:text-gray-400">Login</Link></li>
+                        <li><Link to="/register" className="hover:text-gray-400">Register</Link></li>
+                        <li><Link to="/post-ad" className="hover:text-gray-400">Post Ad</Link></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
