8caf6a21ca9cee48beffe91f696c25618cec86ba
Mahdi Touiserkani <mahditouserkani@yahoo.com>
2025-05-14 01:25:29 +0330

    Set up JSON server with authentication by updating package.json, defining user data in db.json, and implementing authentication middleware in server.js for user management.

---
+++ server.js
@@ -0,0 +1,14 @@
+import jsonServer from 'json-server';
+import auth from 'json-server-auth';
+
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(auth);
+server.use(router);
+
+server.listen(3000, () => {
+  console.log('JSON Server is running');
+});
